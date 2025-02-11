033e443b8f4eda971f03e7cdf1b4a89af166b90f
B2103505 <phamvulamvip@gmail.com>
2025-02-11 19:31:16 +0700

    Design Header

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -1,55 +1,101 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './HomeHeader.scss';
+import logo from '../../assets/Logo.webp';
 
 class HomeHeader extends Component {
 
     render() {
         return (
-            <div className='home-header-container'>
-                <div className='home-header-content'>
-                    <div className='left-content'>
-                        <i className="fas fa-bars"></i>
-                        <div className='header-logo'>
-                        </div>
-                    </div>
-
-                    <div className='center-content'>
-                        <div className='child-content'>
-                            <div>
-                                <b>Tất cả</b>
+            <React.Fragment>
+                <div className='home-header-container'>
+                    <div className='home-header-content'>
+                        <div className='left-content'>
+                            <i className="fas fa-bars"></i>
+                            <div className='header-logo'>
+                                <a href="#">
+                                    <img src={logo} alt="Logo" />
+                                </a>
                             </div>
                         </div>
 
-                        <div className='child-content'>
-                            <div>
-                                <b>Tại nhà</b>
+                        <div className='center-content'>
+                            <div className='content-bar'>
+                                <div className='child-content'>
+                                    <div>
+                                        <b>Tất cả</b>
+                                    </div>
+                                </div>
+
+                                <div className='child-content'>
+                                    <div>
+                                        <b>Tại nhà</b>
+                                    </div>
+                                </div>
+
+                                <div className='child-content'>
+                                    <div>
+                                        <b>Tại viện</b>
+                                    </div>
+                                </div>
+
+                                <div className='child-content'>
+                                    <div>
+                                        <b>Sống khỏe</b>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className='search-bar'>
+                                <input type="text" className="search-input" placeholder="Tìm kiếm">
+                                </input>
+                                <i className="fas fa-search"></i>
                             </div>
                         </div>
 
-                        <div className='child-content'>
-                            <div>
-                                <b>Tại viện</b>
+                        <div className='right-content'>
+                            <div className='cooperation'>
+                                <i className="far fa-handshake"></i>
+                                <p>Hop tac</p>
                             </div>
-                        </div>
 
-                        <div className='child-content'>
-                            <div>
-                                <b>Sống khỏe</b>
+                            <div className='schedule'>
+                                <i className="far fa-clock"></i>
+                                <p>Lich Hen</p>
                             </div>
-                        </div>
-                    </div>
 
-                    <div className='right-content'>
-                        <div className='support'>
-                            <i className="far fa-question-circle">Hỗ trợ</i>
-                        </div>
-                        <div className='flag-langue'>
-                            VN
+                            <div className='flag-language'>
+
+                                Viet Nam
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+
+                <div className='home-header-banner'>
+                    <div className="introduce-section">
+
+                        <h2>NƠI KHỞI NGUỒN SỨC KHỎE</h2>
+                        <h2>HÃY CHUNG TAY BẢO VỆ ĐỂ LUÔN CÓ SỨC KHỎE DÂNG TRÀO</h2>
+
+                        <div className='Search-Recommend'>
+                            <div className='Search-Area'>
+                                <input type="text" className="search-input" placeholder="Tìm kiếm">
+                                </input>
+                                <i className="fas fa-paper-plane"></i>
+                            </div>
+
+                            <div className='Recommend-Area'>
+                                <i className="fas fa-hospital"></i>
+                                Chon Benh Vien
+                            </div>
+
+                        </div>
+                    </div>
+
+
+                </div>
+            </React.Fragment>
         )
     }
 
