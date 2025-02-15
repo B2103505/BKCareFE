3b2f5e67e013b17a9533a224564978207f21df9d
Cirioto <146433860+Cirioto@users.noreply.github.com>
2025-02-15 20:34:12 +0700

    Update_FE

--- src/containers/HomePage/Section/Section.js
+++ src/containers/HomePage/Section/Section.js
@@ -2,31 +2,38 @@ import React, { useState } from "react";
 import DragScroll from "./DragScroll";
 import ModalBK from "./Modal/ModalBK";
 
+import { FormattedMessage } from "react-intl";
+import "./SpecialtySection.scss";
+
 function Section({ title, items }) {
-    const [selectedService, setSelectedService] = useState(null);
+  const [selectedService, setSelectedService] = useState(null);
 
-    return (
-        <div className="services-overview">
-            <h1>{title}</h1>
-            <DragScroll>
-                <div className="specialty-links-container">
-                    {items.map((item, index) => (
-                        <div className="specialty-container" key={index}>
-                            <button onClick={() => setSelectedService(item)}>
-                                <div>
-                                    <img src={item.img} alt={item.text} />
-                                </div>
-                                <span>{item.text}</span>
-                            </button>
-                        </div>
-                    ))}
+  return (
+    <div className="services-overview">
+      <h1>
+        <FormattedMessage id={title} />
+      </h1>
+      <DragScroll>
+        <div className="specialty-links-container">
+          {items.map((item, index) => (
+            <div className="specialty-container" key={index}>
+              <button onClick={() => setSelectedService(item)}>
+                <div>
+                  <img src={item.img} alt={item.text} />
                 </div>
-            </DragScroll>
-
-            {/* Hiển thị modal nếu có dịch vụ được chọn */}
-            <ModalBK service={selectedService} onclose={() => setSelectedService(null)} />
+                <span>
+                  <FormattedMessage id={item.text} />
+                </span>
+              </button>
+            </div>
+          ))}
         </div>
-    );
+      </DragScroll>
+
+      {/* Hiển thị modal nếu có dịch vụ được chọn */}
+      <ModalBK service={selectedService} onclose={() => setSelectedService(null)} />
+    </div>
+  );
 }
 
 export default Section;
