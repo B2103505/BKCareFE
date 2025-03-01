50bea1ebb51adbcbf380f37d3366a8ed4457532c
Cirioto <146433860+Cirioto@users.noreply.github.com>
2025-03-01 17:16:33 +0700

    Chỉnh sửa hơi bị nhiều
    Chuyển các mảng dữ liệu như specialties, services, hospital sang các file Data.js riêng lẻ trong thư mục Data.
    
    Đã lấy được thông tin và hiển thị ModalService khi nhấn vào.

---
+++ src/containers/HomePage/Data/specialtyData.js
@@ -0,0 +1,13 @@
+const specialties = [
+  { img: "/specialty-icon/1.webp", text: "specialty.musculoskeletal" },
+  { img: "/specialty-icon/2.webp", text: "specialty.neurology" },
+  { img: "/specialty-icon/3.webp", text: "specialty.digestive" },
+  { img: "/specialty-icon/4.webp", text: "specialty.cardiology" },
+  { img: "/specialty-icon/5.webp", text: "specialty.ent" },
+  { img: "/specialty-icon/6.webp", text: "specialty.spine" },
+  { img: "/specialty-icon/7.webp", text: "specialty.traditional_medicine" },
+  { img: "/specialty-icon/8.webp", text: "specialty.acupuncture" },
+  { img: "/specialty-icon/9.webp", text: "specialty.obstetrics_gynecology" },
+];
+
+export default specialties;
--- src/containers/HomePage/Section/HospitalSection.js
+++ src/containers/HomePage/Section/HospitalSection.js
@@ -1,16 +1,8 @@
 import React from "react";
 import Section from "./Section";
+import hospitals from "../Data/hospitalData";
 
 function HospitalSection() {
-  const hospitals = [
-    { img: "/hospital/1.webp", text: "hospital.vietduc" },
-    { img: "/hospital/2.webp", text: "hospital.choray" },
-    { img: "/hospital/3.webp", text: "hospital.doctorcheck" },
-    { img: "/hospital/4.webp", text: "hospital.fv" },
-    { img: "/hospital/5.webp", text: "hospital.anviet" },
-    { img: "/hospital/6.webp", text: "hospital.anphuoc" },
-  ];
-
   return <Section title="hospital.name" items={hospitals} />;
 }
 
