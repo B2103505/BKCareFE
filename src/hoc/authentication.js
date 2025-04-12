68d48c9fab034832ce036dc2c7e58f50e1c11fab
Cirioto <146433860+Cirioto@users.noreply.github.com>
2025-04-12 07:53:31 +0700

    Chỉnh lại route trong trạng thái đã đăng nhập

--- src/hoc/authentication.js
+++ src/hoc/authentication.js
@@ -4,15 +4,23 @@ import { connectedRouterRedirect } from "redux-auth-wrapper/history4/redirect";
 const locationHelper = locationHelperBuilder({});
 
 export const userIsAuthenticated = connectedRouterRedirect({
-    authenticatedSelector: state => state.user.isLoggedIn,
-    wrapperDisplayName: 'UserIsAuthenticated',
-    redirectPath: '/login'
+  authenticatedSelector: (state) => state.user.isLoggedIn,
+  wrapperDisplayName: "UserIsAuthenticated",
+  redirectPath: "/login",
 });
 
 export const userIsNotAuthenticated = connectedRouterRedirect({
-    // Want to redirect the user when they are authenticated
-    authenticatedSelector: state => !state.user.isLoggedIn,
-    wrapperDisplayName: 'UserIsNotAuthenticated',
-    redirectPath: (state, ownProps) => locationHelper.getRedirectQueryParam(ownProps) || '/',
-    allowRedirectBack: false
+  // Want to redirect the user when they are authenticated
+  authenticatedSelector: (state) => !state.user.isLoggedIn,
+  wrapperDisplayName: "UserIsNotAuthenticated",
+
+  // Chỉnh lại route cho trường hợp chạy đường dẫn login trong trạng thái đã đăng nhập
+  redirectPath: (state, ownProps) => {
+    const roleId = state.user?.userInfo?.roleId;
+    if (roleId === "R1") return "/system/user-manage";
+    if (roleId === "R2") return "/doctor/manage-schedule";
+    return "/home";
+  },
+
+  allowRedirectBack: false,
 });
